78b2e0e37c3a0b6d16baee76b1ab8483a9abd8b8
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-14 11:37:25 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,66 @@
-
+let hand = 0
+let score = 0
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . #
+            `)
+    }
+    hand = Math.randomRange(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (score > 0) {
+        score += -1
+        basic.showNumber(score)
+    }
+})
